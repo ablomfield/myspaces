ca6ace4ef5d27cc6298f39bd1db88c01bf4ef53f
Adam Blomfield <ablomfield@presidio.com>
2025-12-03 20:51:58 -0600

    index

--- html/index.php
+++ html/index.php
@@ -83,13 +83,7 @@ include($_SERVER['DOCUMENT_ROOT'] . "/includes/settings.php");
                     }
                     $getroomsjson = substr($getroomsresponse, $header_size);
                     $getroomsarr = json_decode($getroomsjson);
-                    //print_r($getroomsarr);
-                    echo("getroomsurl = " . $getroomsurl . "<br><br>\n");
-                    echo("relurl = " . $relurl . "<br><br>\n");
                     $itemcount = count((array)$getroomsarr->items);
-                    //$itemcount = 1000;
-                    echo ("Retrieved " . $itemcount . " items.<br>\n");
-
                     for ($roomindex = 0; $roomindex <= $itemcount - 1; $roomindex++) {
                         if (isset($getroomsarr->items[$roomindex]->id)) {
                             $roomid = $getroomsarr->items[$roomindex]->id;
